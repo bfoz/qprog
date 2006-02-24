0fcd72814d963b391aa51a61ec8b7a681963ab39
Brandon Fosdick <bfoz@bfoz.net>
2006-02-24 23:15:52 +0000

    Rearrange handle_open_text() a bit Put error handling back (accidentally commited the wrong version before)

--- src/centralwidget.cc
+++ src/centralwidget.cc
@@ -523,53 +523,61 @@ void handle_open_new_text(intelhex::hex_data& HexData)
 	FSRef	ref;
 	LSApplicationParameters	app_parms = {0, kLSLaunchNoParams, &ref, NULL, NULL, NULL, NULL};
 	ProcessSerialNumber	psn;
-	AppleEvent openEvent, createEvent, replyEvent;
-	AEDesc	docSpec, insertLoc;
-	std::ostringstream text;
 
 	//Find the preferred application for text files
 	if( LSGetApplicationForInfo('TEXT', kLSUnknownCreator, NULL, kLSRolesAll, &ref, NULL) != noErr )
-		std::cout << "Error\n";
-	LSOpenApplication(&app_parms, &psn);	//Launch the application
-
-	HexData.write(text);
-//	std::cout << "text => " << text.str() << std::endl;
-
+	{
+		QMessageBox::critical(NULL, "Error", "Could not find preferred text editor");
+		return;
+	}
+	
+	if( LSOpenApplication(&app_parms, &psn) != noErr )	//Launch the application
+	{
+		QMessageBox::critical(NULL, "Error", "Could not open preferred text editor");
+		return;
+	}
+	
+	AppleEvent createEvent;
 	//Build an event to open a new document
-	err = AEBuildAppleEvent(kAECoreSuite, kAECreateElement, typeProcessSerialNumber, &psn, sizeof(psn), kAutoGenerateReturnID, kAnyTransactionID, &createEvent, NULL, "kocl:type(docu)");
-/*	Handle pretty_handle;
-	err = AEPrintDescToHandle(&createEvent, &pretty_handle);
-	if( err == noErr )
-		printf("event: %s\n", *pretty_handle);
-	else
-		printf("bad pretty print %d\n", err);
+	if( AEBuildAppleEvent(kAECoreSuite, kAECreateElement, typeProcessSerialNumber, &psn, sizeof(psn), kAutoGenerateReturnID, kAnyTransactionID, &createEvent, NULL, "kocl:type(docu)") != noErr )
+	{
+		AppleEvent replyEvent;
+
+/*		Handle pretty_handle;
+		err = AEPrintDescToHandle(&createEvent, &pretty_handle);
+		if( err == noErr )
+			printf("event: %s\n", *pretty_handle);
+		else
+			printf("bad pretty print %d\n", err);
 */
-	//Send an Apple Event to open a new document
-	err = AESendMessage(&createEvent, &replyEvent, kAEWaitReply, kAEDefaultTimeout);
-	if( err != noErr )
-		std::cout << "Error sending create event " << err << "\n";
-	AEGetParamDesc(&replyEvent, keyDirectObject, typeWildCard, &docSpec);
+		//Send an Apple Event to open a new document
+		if( AESendMessage(&createEvent, &replyEvent, kAEWaitReply, kAEDefaultTimeout) != noErr )
+		{
+			AppleEvent openEvent;
+			AEDesc	docSpec, insertLoc;
+			std::ostringstream text;
 
-	//Build an Apple Event to send the text to the editor
-	err = AEBuildAppleEvent(kAECoreSuite, kAECreateElement, typeProcessSerialNumber, &psn, sizeof(psn), kAutoGenerateReturnID, kAnyTransactionID, &openEvent, NULL, "kocl:type(cpar), data:TEXT(@)", text.str().c_str());
-//	AEPutAttributeDesc(&openEvent, keySubjectAttr, &docSpec);	//Add the doc spec as an attribute
-	AEBuildDesc(&insertLoc, NULL, "insl{kobj:@, kpos:end}", &docSpec);	//Make an insert location
-	AEPutParamDesc(&openEvent, keyAEInsertHere, &insertLoc);	//Insert the insert location
-	
-	if( err != aeBuildSyntaxNoErr )
-		std::cout << "Error building event " << err << "\n";
+			//Get the doc spec from the reply event
+			AEGetParamDesc(&replyEvent, keyDirectObject, typeWildCard, &docSpec);
+			AEDisposeDesc(&createEvent);	//The create event is no longer needed
 
-	//Send an Apple Event to append the new text
-	err = AESendMessage(&openEvent, NULL, kAENoReply, kAEDefaultTimeout);
-	if( err != noErr )
-		std::cout << "Error sending event " << err << "\n";
-	
-	//Cleanup
-	AEDisposeDesc(&docSpec);
-	AEDisposeDesc(&insertLoc);
-	AEDisposeDesc(&openEvent);
-	AEDisposeDesc(&createEvent);
-	AEDisposeDesc(&replyEvent);
+			//Build an Apple Event to send the text to the editor
+			HexData.write(text);
+			AEBuildAppleEvent(kAECoreSuite, kAECreateElement, typeProcessSerialNumber, &psn, sizeof(psn), kAutoGenerateReturnID, kAnyTransactionID, &openEvent, NULL, "kocl:type(cpar), data:TEXT(@)", text.str().c_str());
+//			AEPutAttributeDesc(&openEvent, keySubjectAttr, &docSpec);	//Add the doc spec as an attribute
+			AEBuildDesc(&insertLoc, NULL, "insl{kobj:@, kpos:end}", &docSpec);	//Make an insert location
+			AEPutParamDesc(&openEvent, keyAEInsertHere, &insertLoc);	//Insert the insert location
+
+			//Send an Apple Event to append the new text
+			AESendMessage(&openEvent, NULL, kAENoReply, kAEDefaultTimeout);
+			
+			//Cleanup
+			AEDisposeDesc(&docSpec);
+			AEDisposeDesc(&insertLoc);
+			AEDisposeDesc(&openEvent);
+			AEDisposeDesc(&replyEvent);
+		}
+	}
 }
 #endif	//Q_OS_DARWIN
 
