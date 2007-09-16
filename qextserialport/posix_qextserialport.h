f9c48a06b8ebd9fba28a1c467ceb71688ab92590
Brandon Fosdick <bfoz@bfoz.net>
2007-09-16 20:32:38 +0000

    Import QextSerialPort 1.0

--- qextserialport/posix_qextserialport.h
+++ qextserialport/posix_qextserialport.h
@@ -14,9 +14,11 @@
 class Posix_QextSerialPort:public QextSerialBase {
 public:
     Posix_QextSerialPort();
+    Posix_QextSerialPort(const Posix_QextSerialPort& s);
     Posix_QextSerialPort(const QString & name);
     Posix_QextSerialPort(const PortSettings& settings);
     Posix_QextSerialPort(const QString & name, const PortSettings& settings);
+    Posix_QextSerialPort& operator=(const Posix_QextSerialPort& s);
     virtual ~Posix_QextSerialPort();
 
     virtual void setBaudRate(BaudRateType);
@@ -33,6 +35,10 @@ public:
     virtual qint64 size() const;
     virtual qint64 bytesAvailable();
 
+    virtual bool getChar(char * c);
+    virtual bool putChar(char c);
+    virtual void ungetChar(char c);
+
     virtual void translateError(ulong error);
 
     virtual void setDtr(bool set=true);
@@ -42,15 +48,9 @@ public:
     virtual qint64 readData(char * data, qint64 maxSize);
     virtual qint64 writeData(const char * data, qint64 maxSize);
 
-private:
-    Posix_QextSerialPort(const Posix_QextSerialPort&) {}
-    Posix_QextSerialPort& operator=(const Posix_QextSerialPort&) {}
-
 protected:
-    QFile Posix_File;
-	 int	fd;
+    QFile* Posix_File;
     struct termios Posix_CommConfig;
-    struct termios save_termios;
     struct timeval Posix_Timeout;
     struct timeval Posix_Copy_Timeout;
 };
